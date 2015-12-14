23103d637f0d775a87d199370b9818911d835707
nrinat <nrinat@ecentricarts.com>
2015-12-14 15:25:53 -0500

    Added remark

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            // Remark
             Console.WriteLine("Hello World");
             Console.Read();
         }
